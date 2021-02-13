a49d55f2a062731000283f51638d13ba10d9af22
Andreas Fritiofson <andreas@fritiofson.net>
2021-02-13 14:18:54 +0100

    Simplify kitchen light (still non-functional)

--- data/kitchen-light.js
+++ data/kitchen-light.js
@@ -1,5 +1,8 @@
 /* eslint-disable */
-const zigbeeHerdsmanConverters = require('zigbee-herdsman-converters');
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const e = exposes.presets;
 
 const device = {
     zigbeeModel: ['DIM0-10V2CH'],
@@ -9,42 +12,44 @@ const device = {
 //    extend: generic.light_onoff_brightness,
     supports: 'on/off, brightness',
     fromZigbee: [
-        zigbeeHerdsmanConverters.fromZigbeeConverters.on_off,
-        zigbeeHerdsmanConverters.fromZigbeeConverters.brightness,
-        zigbeeHerdsmanConverters.fromZigbeeConverters.ignore_basic_report
+        fz.on_off,
+        fz.brightness,
+        fz.ignore_basic_report
     ],
     toZigbee: [
-        zigbeeHerdsmanConverters.toZigbeeConverters.light_onoff_brightness,
-        zigbeeHerdsmanConverters.toZigbeeConverters.ignore_transition,
-        zigbeeHerdsmanConverters.toZigbeeConverters.ignore_rate,
-        zigbeeHerdsmanConverters.toZigbeeConverters.effect,
-        zigbeeHerdsmanConverters.toZigbeeConverters.light_brightness_move,
-        zigbeeHerdsmanConverters.toZigbeeConverters.light_brightness_step,
+        tz.light_onoff_brightness,
+        tz.ignore_transition,
+        tz.ignore_rate,
+        tz.effect,
+        tz.light_brightness_move,
+        tz.light_brightness_step,
     ],
     exposes: [
-        zigbeeHerdsmanConverters.exposes.presets.light_brightness().withEndpoint('l1'),
-        zigbeeHerdsmanConverters.exposes.presets.light_brightness().withEndpoint('l2'),
-        zigbeeHerdsmanConverters.exposes.light().withEndpoint('l3')
+        e.light_brightness().withEndpoint('l1'),
+        e.light_brightness().withEndpoint('l2'),
+        exposes.light().withEndpoint('l3')
     ],
     endpoint: (device) => {
         return {l1: 1, l2: 2, l3: 3};
     },
     meta: {
         configureKey: 1,
-        multiEndpoint: true
+        multiEndpoint: true,
+        disableDefaultResponse: true
     },
-    configure: async (device, coordinatorEndpoint) => {
-        const endpoint1 = device.getEndpoint(1);
-        const endpoint2 = device.getEndpoint(2);
-        const endpoint3 = device.getEndpoint(3);
-        await bind(endpoint1, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
-        await configureReporting.onOff(endpoint1);
-        await configureReporting.brightness(endpoint1);
-        await bind(endpoint2, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
-        await configureReporting.onOff(endpoint2);
-        await configureReporting.brightness(endpoint2);
-        await bind(endpoint3, coordinatorEndpoint, ['genOnOff']);
-        await configureReporting.onOff(endpoint3);
-    },
+//    configure: async (device, coordinatorEndpoint) => {
+//        const endpoint1 = device.getEndpoint(1);
+//        const endpoint2 = device.getEndpoint(2);
+//        const endpoint3 = device.getEndpoint(3);
+//        await bind(endpoint1, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
+//        await configureReporting.onOff(endpoint1);
+//        await configureReporting.brightness(endpoint1);
+//        await bind(endpoint2, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
+//        await configureReporting.onOff(endpoint2);
+//        await configureReporting.brightness(endpoint2);
+//        await bind(endpoint3, coordinatorEndpoint, ['genOnOff']);
+//        await configureReporting.onOff(endpoint3);
+//    },
 };
+
 module.exports = device;
