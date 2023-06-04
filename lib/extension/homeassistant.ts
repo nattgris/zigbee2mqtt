bccbae3b1ad5b13b19d8c51056ffaade502fda80
Andreas Fritiofson <andreas.fritiofson@gmail.com>
2023-06-04 15:56:24 +0200

    Disable legacy HA sensor for settable enum
    Only the select entity is needed.
    
    Perhaps could be guarded by a configuration to enable legacy entities?

--- lib/extension/homeassistant.ts
+++ lib/extension/homeassistant.ts
@@ -890,7 +890,7 @@ export default class HomeAssistant extends Extension {
             const valueTemplate = firstExpose.access & ACCESS_STATE ?
                 `{{ value_json.${firstExpose.property} }}` : undefined;
 
-            if (firstExpose.access & ACCESS_STATE) {
+            if (firstExpose.access & ACCESS_STATE && !(firstExpose.access & ACCESS_SET)) {
                 discoveryEntries.push({
                     type: 'sensor',
                     object_id: firstExpose.property,
